6bed3d983d3b3024aa43485378d453da037b24df
Ceramicskate0 <ceramicskate0@users.noreply.github.com>
2023-08-11 13:11:37 -0400

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,6 +39,9 @@
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
+            this.notifyIcon1.BalloonTipClicked += new System.EventHandler(this.notifyIcon1_BalloonTipClicked_1);
+            this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
             // Form1
--- Form1.cs
+++ Form1.cs
@@ -15,32 +15,48 @@ namespace BallonAlert
     {
         public Form1(string[] args)
         {
-            InitializeComponent();
-            string text="";
-            for(int x=1;x<args.Length;++x)
+            try
             {
-                text += " "+args[x];
+                InitializeComponent();
+                string text = "";
+                for (int x = 1; x < args.Length; ++x)
+                {
+                    text += " " + args[x];
+                }
+                this.notifyIcon1.BalloonTipText = text;
+                this.notifyIcon1.BalloonTipTitle = "Alert";
+                this.notifyIcon1.Visible = true;
+                this.notifyIcon1.Text = text;
+                this.notifyIcon1.ShowBalloonTip(60);
             }
-            this.notifyIcon1.BalloonTipText = text;
-            this.notifyIcon1.BalloonTipTitle = "Alert";
-            this.notifyIcon1.Visible = true;
-            this.notifyIcon1.Text = text;
-            this.notifyIcon1.ShowBalloonTip(60);
+            catch
+            {
+                Environment.Exit(1);
+            }
         }
 
         private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            Process.Start("mmc.exe","\"C:\\WINDOWS\\system32\\eventvwr.msc\" /s");
+            Process.Start("cmd","%windir%\\system32\\eventvwr.msc /s");
+            Environment.Exit(0);
+        }
+
+        private void notifyIcon1_BalloonTipClicked_1(object sender, EventArgs e)
+        {
+            Process.Start("cmd", "%windir%\\system32\\eventvwr.msc /s");
+            Environment.Exit(0);
+        }
+
+        private void notifyIcon1_Click(object sender, EventArgs e)
+        {
+            Process.Start("cmd", "%windir%\\system32\\eventvwr.msc /s");
+            Environment.Exit(0);
         }
 
         private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
         {
+            Process.Start("%windir%\\system32\\eventvwr.msc /s");
             Environment.Exit(0);
-        }
-
-        private void notifyIcon1_BalloonTipClicked(object sender, EventArgs e)
-        {
-            Process.Start("mmc.exe", "\"C:\\WINDOWS\\system32\\eventvwr.msc\" /s");
         }
     }
 }
